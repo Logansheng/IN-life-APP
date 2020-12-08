91e31c1adcb06f82aea6813decbe41bb87a72822
loganyu <logan0926@163.com>
2020-12-08 15:55:41 +0800

    创建三个页面

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Money</router-link> |
+      <router-link to="/labels">Labels</router-link>|
+      <router-link to="/statistics">Statistics</router-link>
     </div>
     <router-view/>
   </div>
