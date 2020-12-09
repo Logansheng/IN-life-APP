26757add4b4020e6ef03bb4f67eaf87e3cadb56e
loganyu <logan0926@163.com>
2020-12-09 10:54:37 +0800

    引入 SVG

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="nav">
-    <router-link to="/">Money</router-link> |
-    <router-link to="/labels">Labels</router-link>|
-    <router-link to="/statistics">Statistics</router-link>
+    <router-link to="/">
+      <Icon name="money"></Icon>
+      Money</router-link> |
+    <router-link to="/labels">
+      <Icon name="labels"></Icon>
+      Labels</router-link>|
+    <router-link to="/statistics">
+      <Icon name="statistics"></Icon>
+      Statistics</router-link>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-name: "Nav"
+name: "Nav",
 }
 </script>
 
