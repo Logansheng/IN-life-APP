f2655e31829c7e46e9e4ee88bb609eca09ede784
loganyu <logan0926@163.com>
2020-12-14 16:41:47 +0800

    标签页样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -2,13 +2,13 @@
   <Layout class="context">
 
     <ol>
-      <li><span>衣</span> <Icon name="right"></Icon></li>
-      <li><span>食</span> <Icon name="right"></Icon></li>
-      <li><span>住</span> <Icon name="right"></Icon></li>
-      <li><span>行</span> <Icon name="right"></Icon></li>
+      <li><span>衣</span> <Icon class="icon" name="right"></Icon></li>
+      <li><span>食</span> <Icon class="icon" name="right"></Icon></li>
+      <li><span>住</span> <Icon class="icon" name="right"></Icon></li>
+      <li><span>行</span> <Icon class="icon" name="right"></Icon></li>
     </ol>
-    <div>
-      <button>新建标签</button>
+    <div class="newTag-wrapper">
+      <button class="newTag">新建标签</button>
     </div>
   </Layout>
 </template>
@@ -21,16 +21,35 @@ name: "Labels"
 
 <style lang="scss" scoped>
 .context{
-
-
   ol{
-
+    background: #fff;
+    padding-left: 16px;
     font-size: 16px;
-    li{
-      border: 1px solid red;
-      background: #fff;
-      min-height: 44px;
+    .icon{
+      height: 16px;
+      width: 16px;
+      color: #666;
+      margin-right: 16px;
     }
+    li{
+      min-height: 44px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1px solid #e6e6e6;
+    }
+  }
+  .newTag{
+    background: #767676;
+    height: 32px;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    padding: 0 16px;
+  }
+  .newTag-wrapper{
+    padding: 32px 0;
+    text-align: center;
   }
 }
 </style>
